59e93622ad1c05347141d83cec0644596f326371
Philip Lee <dltkdaud1992@naver.com>
2024-02-14 19:37:38 +0900

    check & delete button added

--- todo.js
+++ todo.js
@@ -11,17 +11,22 @@
 // 1. 유저가 인풋 입력하는 것을 가져오기
 let taskInput = document.getElementById('task-input')
 
-// 2. 버튼 누르는 것을 가져오기
+// 2. "추가하기" 버튼 누르는 것을 가져오기
 let addButton = document.getElementById('addBtn');
 
 // 3. 할 일 추가시 저장될 배열 array 만들기
 let taskList = [];
 
 
+
 // (2) 이벤트 리스너들
 
 // 1. 추가하기 버튼 누르면 addTask 함수실행
 addButton.addEventListener("click", addTask);
+
+// 2. 체크버튼 클릭하는 순간 isComplete 을 true 로 바꿔주고
+    // - true이면 끝난걸로 간주하고 밑줄 보여죽
+    // - false이면 안끝난것으로 간주하고 그대로
 
 
 
@@ -32,12 +37,20 @@ function addTask() {
     // console.log("clicked");
     // addTask 클릭 잘 동작 확인
 
-    let taskContent = taskInput.value
+    // let taskContent = taskInput.value
     // taskInput에 입력되는 value를 taskContent 변수에 저장
     
-    taskList.push(taskContent);
+    let task = {
+        id: randomID(),
+        taskContent: taskInput.value,
+        isComplete:false
+    }
+
+    taskList.push(task);
     // taskContent를 taskList 변수에 저장
-    console.log(taskContent)
+    // console.log(taskContent)
+    taskInput.value = "";
+    // 입력 후 남아있는 텍스트 사라지게 하기!
     render();
 }
  
@@ -48,16 +61,61 @@ function addTask() {
     // string 저장 변수 만듬
 
     for(let i=0; i<taskList.length; i++) {
-        resultHTML += `<div class="task">
-        <div>${taskList[i]}</div>
+        
+        if(taskList[i].isComplete == true) {
+            resultHTML += `<div class="task-checked">
+        <div class="task-done">${taskList[i].taskContent}</div>
         <div class="buttons">
-            <button>Check</button>
-            <button>Delete</button>
+            <button  onclick="toggleComplete('${taskList[i].id}')"><span class="material-symbols-outlined">
+            refresh
+            </span></button>
+            <button onclick="deleteTask('${taskList[i].id}')"><span class="material-symbols-outlined">
+            delete
+            </span></button>
         </div>
     </div>`
-    }
+        } else {
+            resultHTML += `<div class="task">
+            <div>${taskList[i].taskContent}</div>
+            <div class="buttons">
+                <button onclick="toggleComplete('${taskList[i].id}')"><span class="material-symbols-outlined">
+                done
+                </span></button>
+                <button onclick="deleteTask('${taskList[i].id}')"><span class="material-symbols-outlined">
+                delete
+                </span></button>
+            </div>
+        </div>`
+        }
+     }
 
     document.querySelector('.taskListBox').innerHTML = resultHTML;
 
  }
 
+function toggleComplete(id) {
+    for(let i=0; i<taskList.length; i++){
+      if(taskList[i].id == id){
+        taskList[i].isComplete = !taskList[i].isComplete;
+        // true ↔ false vice-versa 로 움직이게하기! 
+        break;
+      }    
+    }
+    render();
+    console.log(taskList)
+}
+
+function deleteTask(id) {
+    for(let i = 0; i<taskList.length; i++) {
+        if(taskList[i].id == id) {
+            taskList.splice(i,1)
+            break;
+        }
+    }
+    render();
+}
+
+
+function randomID(){
+    return Math.random().toString(36).substring(2, 11);
+}
