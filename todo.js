daa0472c64bfb0144ba43742d62a94890e2c3952
Philip Lee <dltkdaud1992@naver.com>
2024-02-15 22:49:43 +0900

    Last updated version

--- todo.js
+++ todo.js
@@ -17,12 +17,36 @@ let addButton = document.getElementById('addBtn');
 // 3. 할 일 추가시 저장될 배열 array 만들기
 let taskList = [];
 
+// 4 filter 버튼 완료 & 미완료 버튼가져오기
+
+let tabs = document.querySelectorAll(".tab-area div")
+
+
+// 5  
+let mode = 'all'
+
+let filterList = [];
+let completedList = [];
+
+
+for(let i = 1; i<tabs.length; i++) {
+  tabs[i].addEventListener("click", function(event)
+  {filter(event)})
+}
+
+
+
 
 
 // (2) 이벤트 리스너들
 
 // 1. 추가하기 버튼 누르면 addTask 함수실행
 addButton.addEventListener("click", addTask);
+taskInput.addEventListener("keydown", function(event) {
+ if(event.keyCode === 13) {
+    addTask(event)
+ }
+});
 
 // 2. 체크버튼 클릭하는 순간 isComplete 을 true 로 바꿔주고
     // - true이면 끝난걸로 간주하고 밑줄 보여죽
@@ -34,18 +58,24 @@ addButton.addEventListener("click", addTask);
 
 // 1. addTask 함수
 function addTask() {
+
+    let taskValue = taskInput.value
+    if(taskValue == "") return alert("할 일을 입력하세요!")
     // console.log("clicked");
     // addTask 클릭 잘 동작 확인
 
     // let taskContent = taskInput.value
     // taskInput에 입력되는 value를 taskContent 변수에 저장
     
+    
+    
     let task = {
         id: randomID(),
         taskContent: taskInput.value,
         isComplete:false
     }
-
+    
+  
     taskList.push(task);
     // taskContent를 taskList 변수에 저장
     // console.log(taskContent)
@@ -57,31 +87,46 @@ function addTask() {
 // 2. taskList를 그려주는 함수
 
  function render() {
+
+    let list = [];
+
+   // 내가 선택한 탭에 따라서
+   if(mode === 'all') {
+    list = taskList;
+   } 
+   else if(mode === 'ongoing' || mode === 'completed') {
+    list = filterList;
+   }
+   // 리스트를 달리 보여준다
+   // all 은 taskList
+   // ongoing & completed 는 filterList
+
     let resultHTML = '';
     // string 저장 변수 만듬
 
-    for(let i=0; i<taskList.length; i++) {
+    for(let i=0; i<list.length; i++) {
         
-        if(taskList[i].isComplete == true) {
+        if(list[i].isComplete == true) {
             resultHTML += `<div class="task-checked">
-        <div class="task-done">${taskList[i].taskContent}</div>
+        <div class="task-done">${list[i].taskContent}</div>
         <div class="buttons">
-            <button  onclick="toggleComplete('${taskList[i].id}')"><span class="material-symbols-outlined">
+            <button  onclick="toggleComplete('${list[i].id}')"><span class="material-symbols-outlined">
             refresh
             </span></button>
-            <button onclick="deleteTask('${taskList[i].id}')"><span class="material-symbols-outlined">
+            <button onclick="deleteTask('${list[i].id}')"><span class="material-symbols-outlined">
             delete
             </span></button>
         </div>
-    </div>`
-        } else {
+    </div>`}  
+     
+        else {
             resultHTML += `<div class="task">
-            <div>${taskList[i].taskContent}</div>
+            <div>${list[i].taskContent}</div>
             <div class="buttons">
-                <button onclick="toggleComplete('${taskList[i].id}')"><span class="material-symbols-outlined">
+                <button onclick="toggleComplete('${list[i].id}')"><span class="material-symbols-outlined">
                 done
                 </span></button>
-                <button onclick="deleteTask('${taskList[i].id}')"><span class="material-symbols-outlined">
+                <button onclick="deleteTask('${list[i].id}')"><span class="material-symbols-outlined">
                 delete
                 </span></button>
             </div>
@@ -109,13 +154,61 @@ function deleteTask(id) {
     for(let i = 0; i<taskList.length; i++) {
         if(taskList[i].id == id) {
             taskList.splice(i,1)
-            break;
+          
         }
     }
     render();
 }
 
 
+function filter(event) {
+    
+    // console.log("filter", event.target.id)
+    // 각 버튼 클릭 시 filter 잘 찍히는지 확인
+    mode = event.target.id
+     filterList = [];
+     completedList =[];
+    
+    if (mode === "all") {
+        // 전체 리스트 보여준다
+        render();
+    } else if (mode === "ongoing"){
+        // 진행중 리스트
+        // task.isComplete = false
+        for (let i=0; i<taskList.length; i++){
+            if(taskList[i].isComplete === false) {
+                filterList.push(taskList[i])
+            }
+        }
+        render();
+        // 진행중 필터 잘되는지 확인
+      
+    } else if(mode === "completed") {
+       for(let i=0; i<taskList.length;i++) {
+        if(taskList[i].isComplete === true) {
+            filterList.push(taskList[i])
+        }
+       }
+    }
+render();
+}
+
+
 function randomID(){
     return Math.random().toString(36).substring(2, 11);
 }
+
+// 언더바 만들기
+
+let underline = document.getElementById('underline');
+// 언더바 가지고 오기
+
+let menuTaps = document.querySelectorAll('#all, #ongoing, #completed');
+
+menuTaps.forEach(menu=>menu.addEventListener("click",(e)=>indicator(e)))
+
+function indicator(e) {
+   underline.style.left = e.currentTarget.offsetLeft + "px";
+   underline.style.width = e.currentTarget.offsetWidth + "px";
+   underline.style.top = e.currentTarget.offsetTop + e.currentTarget.offsetHeight + "px";
+} 
